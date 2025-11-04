62002d3355cd0917cf1dd4dce626d54285d72a46
thejon07 <jojosingh568@gmail.com>
2025-11-04 13:35:39 +0600

    fix the issue of back to school page

--- src/app/(public)/back-to-school/component/Streaming.jsx
+++ src/app/(public)/back-to-school/component/Streaming.jsx
@@ -1,84 +1,11 @@
-"use client"
-import React, { useState } from 'react'
-
+import React from 'react'
 
 function Streaming() {
-    const [data, setData] = useState([])
-    const [toggle, setIsToggle] = useState(false)
-    const [category, setCategory] = useState()
-
-    const handleproducts = async () => {
-        const products = await fetch("/api/sale").then((res) => res.json())
-        console.log(products.data)
-        setData(products.data)
-    }
-
-    const handletoggle = () => {
-        setIsToggle(prev => !prev)
-    }
-
-
-    useEffect(() => {
-        handleproducts()
-    }, [])
-
-    const filtereddata = category
-        ? data?.filter(product => product.category?.toLowerCase() === category.toLowerCase())
-        : data
-
     return (
-        <div>
-            {toggle && (
-                <div className='fixed inset-0 z-40'>
-
-                    <div
-                        className='absolute inset-0 bg-black/50'
-                        onClick={handletoggle}
-                    ></div>
-
-
-                    <div className='w-full bebas-neue-regular-2 sm:w-[400px] h-full bg-white fixed left-0 top-0 z-50 px-6 py-4 flex flex-col justify-between'>
-                        <div>
-                            <div className='font-semibold flex justify-between items-center'>
-                                <h1 className='text-lg'>FILTER & SORT</h1>
-                                <button onClick={handletoggle} className='text-3xl font-bold'>×</button>
-                            </div>
-
-                            <div>
-                                <h2 className='font-semibold my-6 border-b py-2 border-gray-400 text-xl'>SORT BY</h2>
-                                <ul>
-                                    <li className='mb-2 border-b pb-2 border-gray-400 text-sm'>PRICE (LOW - HIGH)</li>
-                                    <li className='flex justify-between pb-2 border-gray-400 border-b'>
-                                        <h3 className='text-sm'>CATEGORY</h3>
-                                        <button className='rotate-180'>{">"}</button>
-                                    </li>
-                                    <li onClick={() => setCategory("male")} className='w-full mt-2 w-full text-center font-semibold border-2 border-black p-2 w-fit cursor-pointer bg-black text-white'>MALE</li>
-                                    <li onClick={() => setCategory("female")} className='w-full text-center mt-2 font-semibold border-2 border-black p-2 w-fit cursor-pointer bg-black text-white'>FEMALE</li>
-                                    <li onClick={() => setCategory(null)} className=' text-center mt-2 text-blue-600 underline cursor-pointer'>Clear Filter</li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <button className='p-2 w-full bg-black text-white font-semibold mt-4'>SEARCH</button>
-                    </div>
-                </div>
-            )}
-            <div className="max-w-[1200px] mx-auto py-10">
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                    {filtereddata?.map((data) => (
-                        <Link href={`/product-details/${data._id}`} key={data._id}>
-                            <div className='w-full h-[380px] px-3 py-4 rounded-md'>
-                                <img src={data.image} alt={data.title} className='w-full object-contain rounded-sm' />
-                                <h1 className='text-base font-semibold mt-2'>{data.title}</h1>
-                                <h2 className='text-sm mt-1'>Price: <span className='font-semibold'>${data.price}</span></h2>
-                                <p className='text-xs mt-2 line-clamp-3'>{data.description}</p>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            </div>
+        <div className=' text-center w-full min-h-[200px] flex items-center justify-center'>
+            <h1 className='text-black'>Loading....</h1>
         </div>
     )
 }
 
-export default Streaming
+export default Streaming;
--- src/app/(public)/back-to-school/page.jsx
+++ src/app/(public)/back-to-school/page.jsx
@@ -1,49 +1,60 @@
 "use client"
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
-import { usePathname } from 'next/navigation'
-import { Suspense } from 'react'
+import Head from 'next/head'
+import { ChevronUp } from "lucide-react"
+import Streaming from "../back-to-school/component/Streaming.jsx"
 
 
-// export const metadata = {
-//     title: "Sale | PotFam",
-//     description:
-//         "Shop the latest clothes and shoes on sale. Find your favorite sneakers, jackets, and more at discounted prices.",
-//     keywords: "sale, clothes, shoes, sneakers, jackets, fashion, discount",
-//     robots: "index, follow",
-// };
 function page() {
+    const [latestData,setLatestData] = useState([])
+    const [selectCategory, setSelectCategory] = useState("")
     const [data, setData] = useState()
     const [toggle, setIsToggle] = useState(false)
-    const [category, setCategory] = useState()
-    const pathname = usePathname()
 
     const handleproducts = async () => {
         const products = await fetch("/api/sale").then((res) => res.json())
         console.log(products.data)
         setData(products.data)
+        setLatestData(products.data)
     }
 
     const handletoggle = () => {
         setIsToggle(prev => !prev)
+    } 
+
+    const handlesetCategory = (value) => {
+        console.log(value)
+        setSelectCategory(value)
+     
     }
 
+    
     useEffect(() => {
         handleproducts()
     }, [])
 
-    const filtereddata = category
-        ? data?.filter(product => product.category?.toLowerCase() === category.toLowerCase())
-        : data
-
+    const handlesearchproducts = () => {
+        const filtereddata = selectCategory ? data?.filter(product => product.category?.toLowerCase() === selectCategory.toLowerCase()): data
+        setLatestData(filtereddata)
+        setIsToggle(false)
+    }
+     
 
     return (
         <>
 
+            <Head>
+                <title >Clothes & Shoes on Sale | PotFam</title>
+                <meta name="description" content="Shop the latest clothes and shoes on sale. Find your favorite sneakers, jackets, and more at discounted prices." />
+                <meta name="keywords" content="sale, clothes, shoes, sneakers, jackets, fashion, discount" />
+                <meta name="robots" content="index, follow" />
+            </Head>
+
 
             <main className='px-4 md:px-10 relative'>
                 {toggle && (
-                    <div className='fixed inset-0 z-40'>
+                    <div className= {`fixed inset-0 z-40`}>
 
                         <div
                             className='absolute inset-0 bg-black/50'
@@ -51,66 +62,73 @@ function page() {
                         ></div>
 
 
-                        <div className='w-full bebas-neue-regular-2 sm:w-[400px] h-full bg-white fixed left-0 top-0 z-50 px-6 py-4 flex flex-col justify-between'>
+                        <div className={` w-full sm:w-[400px] h-full bg-white fixed left-0 top-0 z-50 px-6 py-4 flex flex-col justify-between`}>
                             <div>
                                 <div className='font-semibold flex justify-between items-center'>
-                                    <h1 className='text-lg'>FILTER & SORT</h1>
-                                    <button onClick={handletoggle} className='text-3xl font-bold'>×</button>
+                                    <h1 className='text-lg bebas-neue-regular'>FILTER & SORT</h1>
+                                    <button onClick={handletoggle} className='text-xl font-bold'>×</button>
                                 </div>
 
                                 <div>
-                                    <h2 className='font-semibold my-6 border-b py-2 border-gray-400 text-xl'>SORT BY</h2>
+                                    <h2 className='font-semibold my-6 border-b py-2 border-gray-400 text-xs'>SORT BY</h2>
                                     <ul>
-                                        <li className='mb-2 border-b pb-2 border-gray-400 text-sm'>PRICE (LOW - HIGH)</li>
                                         <li className='flex justify-between pb-2 border-gray-400 border-b'>
-                                            <h3 className='text-sm'>CATEGORY</h3>
-                                            <button className='rotate-180'>{">"}</button>
+                                            <h3 className='text-xl bebas-neue-regular'>CATEGORY</h3>
+                                            <button className='rotate-180'>
+                                                <ChevronUp />
+                                            </button>
                                         </li>
-                                        <li onClick={() => setCategory("male")} className='w-full mt-2 w-full text-center font-semibold border-2 border-black p-2 w-fit cursor-pointer bg-black text-white'>MALE</li>
-                                        <li onClick={() => setCategory("female")} className='w-full text-center mt-2 font-semibold border-2 border-black p-2 w-fit cursor-pointer bg-black text-white'>FEMALE</li>
-                                        <li onClick={() => setCategory(null)} className=' text-center mt-2 text-blue-600 underline cursor-pointer'>Clear Filter</li>
+                                        <li onClick={(e) => handlesetCategory(e.target.innerText)} className={`${selectCategory == "MALE"?"bg-black text-white":""} mt-2 font-semibold border-1 w-full border-black p-2 w-fit cursor-pointer bebas-neue-regular`}>MALE</li>
+                                        <li onClick={(e) => handlesetCategory(e.target.innerText)} className={` mt-2 font-semibold border-1 w-full border-black p-2 w-fit cursor-pointer bebas-neue-regular ${selectCategory=="FEMALE"?"text-white bg-black":""}`}>FEMALE</li>
+                                        <li onClick={() =>  selectCategory(null)} className='text-xs mt-2 text-blue-600 underline cursor-pointer'>Clear Filter</li>
                                     </ul>
                                 </div>
                             </div>
 
-                            <button className='p-2 w-full bg-black text-white font-semibold mt-4'>SEARCH</button>
+                            <button onClick={handlesearchproducts} className='p-2 w-full border border-1 font-semibold bebas-neue-regular border-black mt-4'>SEARCH</button>
                         </div>
                     </div>
                 )}
 
                 {/* Breadcrumb & Heading */}
-                <ul className='flex gap-1 py-10 text-sm flex-wrap'>
+                <ul className='flex gap-1 py-10 text-sm flex-wrap bebas-neue-regular'>
                     <li className=' border-b border-black'>Home</li>
-                    <li>{pathname}</li>
+                    <li>/</li>
+                    <li>back to school</li>
                 </ul>
 
                 <div className='flex justify-between items-center flex-wrap gap-4'>
-                    <h1 className='text-3xl md:text-4xl font-semibold bebas-neue-regular-2'>Back to School Clothes & Shoes</h1>
+                    <h1 className='text-3xl md:text-4xl font-semibold bebas-neue-regular'>Clothes & Shoes on Sale</h1>
                     <button className='px-4 py-2 border-2 border-black bebas-neue-regular' onClick={handletoggle}>FILTER & SORT</button>
                 </div>
 
                 {/* Description */}
-                <section className='py-6 text-sm'>
+                <section className='py-6 text-sm text-gray-600 bebas-neue-regular [word-spacing:4px]'>
                     <p>Find your new favorite tee, jacket, jersey, tights or sneakers shopping clothes and shoes on sale at adidas. With deals like this, you can stock up.</p>
                 </section>
 
                 {/* Product Grid */}
-                <Suspense fallback={<p className='text-black'>Loading slow component...</p>}>
-                    <div className="max-w-[1200px] mx-auto py-10">
+
+                <div className="max-w-[1200px] mx-auto py-10">
+                    {latestData?.length > 0 ?
                         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                            {filtereddata?.map((data) => (
-                                <Link href={`/product-details/${data._id}`} key={data._id}>
-                                    <div className='w-full min-h-[380px] px-3 py-4 sm:my-0 rounded-md'>
+
+                            {latestData?.map((data) => (
+                                <Link href={`/product-details/${data._id}`} key={data.id}>
+                                    <div className='w-full h-[380px] px-3 py-4 rounded-md my-2 sm:my-0'>
                                         <img src={data.image} alt={data.title} className='w-full object-contain rounded-sm' />
                                         <h1 className='text-base font-semibold mt-2'>{data.title}</h1>
                                         <h2 className='text-sm mt-1'>Price: <span className='font-semibold'>${data.price}</span></h2>
                                         <p className='text-xs mt-2 line-clamp-3'>{data.description}</p>
                                     </div>
                                 </Link>
-                            ))}
+                            ))
+                            }
                         </div>
-                    </div>
-                </Suspense>
+                        :
+                        <Streaming />
+                    }
+                </div>
 
             </main>
         </>
